624270541920664165a7d6c61aff4eff0bd6e173
YuliaKorolenko <korollenkoyullia@gmail.com>
2026-03-20 18:06:49 +0100

    Rewrite to AbstractFirPropertyInitializationChecker and to ControlFlowGraphVisitorVoid

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
@@ -73,7 +73,6 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
         FirOperatorModifierChecker,
         FirTailrecFunctionChecker,
         FirVersionOverloadsChecker,
-        FirCapturedVariableStabilityFunctionChecker,
     )
 
     override val simpleFunctionCheckers: Set<FirSimpleFunctionChecker> = setOf(
@@ -195,6 +194,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
 
     override val variableAssignmentCfaBasedCheckers: Set<AbstractFirPropertyInitializationChecker> = setOf(
         FirPropertyInitializationAnalyzer,
+        FirCapturedVariableStabilityFunctionChecker,
     )
 
     override val typeParameterCheckers: Set<FirTypeParameterChecker> = setOf(
