42437985a9d3836df108ca473f6a958f581685fb
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-05-13 18:31:19 +0400

    [PSI] Move constructor delegation mutation to the service
    Move the implicit-to-explicit secondary-constructor delegation rewrite behind KtPsiMutationService.
    
    KT-85052

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtPsiMutationService.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtPsiMutationService.kt
@@ -196,6 +196,11 @@ interface KtPsiMutationService {
      */
     fun setPropertyInitializer(property: KtProperty, initializer: KtExpression?): KtExpression?
 
+    /**
+     * Replaces the implicit delegation call in [constructor] with an explicit `this()` or `super()` call.
+     */
+    fun replaceImplicitDelegationCallWithExplicit(constructor: KtSecondaryConstructor, isThis: Boolean): KtConstructorDelegationCall
+
     @KtNonPublicApi
     companion object {
         /**
