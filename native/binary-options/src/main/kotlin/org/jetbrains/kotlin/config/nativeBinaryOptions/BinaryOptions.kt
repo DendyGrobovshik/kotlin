ce269896e88b98bd744ba0955617f6a4586c674b
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-05-19 20:19:35 +0200

    [K/N] Introduce runLLVMPassesInCompiler binary option
    This option will be used to gate new C++ passes.
    The new passes are enabled by default, this will allow
    disabling them, if any regressions arise.
    
    ^KT-84060

--- native/binary-options/src/main/kotlin/org/jetbrains/kotlin/config/nativeBinaryOptions/BinaryOptions.kt
+++ native/binary-options/src/main/kotlin/org/jetbrains/kotlin/config/nativeBinaryOptions/BinaryOptions.kt
@@ -122,6 +122,13 @@ object BinaryOptions : BinaryOptionRegistry() {
      * Generate a macOS Catalyst binary for the given architecture
      */
     val macabi by booleanOption()
+
+    /**
+     * When `true`, llvm postprocessing will be done in Kotlin compiler;
+     * when `false`, new custom llvm C++ passes will be used.
+     * This option exists as a workaround, if new passes introduce regressions.
+     */
+    val runLLVMPassesInCompiler by booleanOption()
 }
 
 open class BinaryOption<T : Any>(
