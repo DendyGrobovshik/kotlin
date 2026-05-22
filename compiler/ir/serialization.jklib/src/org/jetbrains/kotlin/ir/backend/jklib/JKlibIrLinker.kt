0251c401ab879d942fa2ec19f9b6892d146e2351
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-05-22 14:21:57 +0200

    [Klib] Only search for symbols within Klibs which declare a given package
    on the 2-nd stage of compilation, instead of probing every included Klib.
    It is expected that usually there is only one Klib per package, and in
    that case the linkage of any given signature should finally have O(1)
    complexity.
    
    This optimization does not include JVM metadata deserializer, as it's
    no longer needed and is meant to be dropped.
    
    #KT-82172 Fixed

--- compiler/ir/serialization.jklib/src/org/jetbrains/kotlin/ir/backend/jklib/JKlibIrLinker.kt
+++ compiler/ir/serialization.jklib/src/org/jetbrains/kotlin/ir/backend/jklib/JKlibIrLinker.kt
@@ -26,6 +26,7 @@ import org.jetbrains.kotlin.library.KotlinLibrary
 import org.jetbrains.kotlin.load.java.descriptors.JavaCallableMemberDescriptor
 import org.jetbrains.kotlin.load.java.descriptors.JavaClassDescriptor
 import org.jetbrains.kotlin.load.java.lazy.descriptors.LazyJavaPackageFragment
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
 @OptIn(ObsoleteDescriptorBasedAPI::class)
@@ -113,6 +114,8 @@ class JKlibIrLinker(
 
         override fun contains(idSig: IdSignature): Boolean = resolveDescriptor(idSig) != null
 
+        override fun getDefinedPackageNames(): Set<FqName>? = null
+
         private val descriptorFinder = DescriptorByIdSignatureFinderImpl(
             moduleDescriptor,
             descriptorMangler,
@@ -181,6 +184,8 @@ class JKlibIrLinker(
 
         override fun contains(idSig: IdSignature): Boolean =
             super.contains(idSig) || descriptorByIdSignatureFinder.findDescriptorBySignature(idSig) != null
+
+        override fun getDefinedPackageNames(): Set<FqName>? = null
 
         override fun tryDeserializeIrSymbol(
             idSig: IdSignature,
