02a6a0a11795681a928c89f0aad604dc7ff28afe
Jose Enrique Estremadoyro Fort <joseefort@google.com>
2026-05-27 13:14:32 -0600

    fixup! [TEST] Introduce compilation caching in MockLibraryUtil

---
+++ compiler/tests-compiler-utils/testFixtures/org/jetbrains/kotlin/test/util/CompiledLibraryCache.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.util
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
+import java.io.File
+import java.util.concurrent.ConcurrentHashMap
+
+class CompiledLibraryCache : Disposable {
+    private val cache = ConcurrentHashMap<String, File>()
+
+    init {
+        try {
+            val disposerClass = Class.forName("org.jetbrains.kotlin.test.ApplicationEnvironmentDisposer")
+            val rootDisposableField = disposerClass.getField("ROOT_DISPOSABLE")
+            val rootDisposable = rootDisposableField.get(null) as Disposable
+            Disposer.register(rootDisposable, this)
+        } catch (e: Throwable) {
+            // Ignore if ApplicationEnvironmentDisposer is not on the classpath
+        }
+    }
+
+    fun getOrCompile(key: String, compile: () -> File): File {
+        val cached = cache[key]
+        if (cached != null) return cached
+
+        synchronized(this) {
+            val doubleCheck = cache[key]
+            if (doubleCheck != null) return doubleCheck
+
+            val compiled = compile()
+            cache[key] = compiled
+            return compiled
+        }
+    }
+
+    override fun dispose() {
+        cache.clear()
+    }
+}
