3dc712a947e17da0c9ceaaf7f3a4b4be24b5487d
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-06-04 09:40:26 +0200

    fixup! [K/N][performance] Move Random usage out of measurement code

--- kotlin-native/performance/ring/src/commonMain/kotlin/org/jetbrains/ring/SplayBenchmark.kt
+++ kotlin-native/performance/ring/src/commonMain/kotlin/org/jetbrains/ring/SplayBenchmark.kt
@@ -259,11 +259,7 @@ class SplayHideName {
     val splayTreePayloadDepth = 5;
     val splayTree = splaySetup()
 
-    private val randomInts = IntArray(1000) { random.nextInt() }
-    private var nextKeyIndex = 0
-    fun generateKey(): Int = randomInts[nextKeyIndex].also {
-        nextKeyIndex = (nextKeyIndex + 1) % randomInts.size
-    }
+    fun generateKey(): Int = random.nextInt()
 
     fun generatePayloadTree(depth: Int, tag: String): Pair<Any, Any> {
         return if (depth == 0) {
